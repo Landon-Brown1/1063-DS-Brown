ccb9ce57841c642e4fb41ad954d0e89cadd31c75
Landon Brown <lmbrown0908@my.msutexas.edu>
2019-09-10 15:06:34 -0500

    making converter to help in computer architecture

--- Practice_Code/9-10.cpp
+++ Practice_Code/9-10.cpp
@@ -49,3 +49,54 @@ int main(){
 
     return 0;
 }
+
+/*
+create a program that acts as a base converter
+
+int main(){
+    stringstream number = 0;
+    int fromBase        = 0;
+    int toBase          = 0;
+    string confirm      = NULL;
+    bool sure           = false;
+    bool flag           = false;
+    
+    while(!sure){
+    
+        cout << "Please enter a base to convert from: ";
+        cin >> fromBase;
+        cout << endl;
+        cout << "A base to convert to: ";
+        cin >> toBase;
+        cout << endl;
+        cout << "And a number to convert: ";
+        cin >> number;
+        cout << endl;
+
+        cout << "Okay, so you want to convert " << number 
+             << " base " << fromBase 
+             << " to base " << toBase << "?" << endl;
+
+        while(!flag){
+            cout << endl << "Type 'y' or 'n'";
+            cin >> confirm;
+            if(confirm == y){
+                sure = true;
+                flag = true;
+            }
+            else if(confirm == n){
+                cout << "Okay, I'll reset your values."
+                flag = true;
+            }
+            else{
+                cout << "Invalid input, please try again." << endl;
+            }
+        
+        } 
+
+    }
+    
+}
+
+
+*/
