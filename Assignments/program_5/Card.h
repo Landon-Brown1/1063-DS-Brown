4a036061927ba6030710db49016679222dbb0301
Landon Brown <lmbrown0908@my.msutexas.edu>
2019-12-05 11:41:08 -0600

    added setters and getters

--- Assignments/program_5/Card.h
+++ Assignments/program_5/Card.h
@@ -12,17 +12,21 @@ class Card{
 private:
     std::string suit;
     char value;
+    Card* next;
+
 public:
     //empty constructor
     Card(){
         suit = "spade";
         value = '2';
+        next = NULL;
     }
 
     //loaded constructor
     Card(std::string s, char c){
         suit = s;
         value = c;
+        next = NULL;
     }
 
     //copy constructor
@@ -32,8 +36,41 @@ public:
     }
 
     //overloaded assignment operator
-    Card& operator= (const Card& c){
-        this->suit = c.suit;
-        this->value = c.value;
+    Card& operator= (const Card& that){
+        if(this != &that){
+            this->suit = that.suit;
+            this->value = that.value;
+        }
+        return *this;
+    }
+
+    //returns card->next
+    Card* _getNext(){
+        return this->next;
+    }
+
+    //sets card->next
+    void _setNext(Card* n){
+        this->next = n;
+    }
+
+    //returns card->suit
+    std::string _getSuit(){
+        return this->suit;
+    }
+
+    //sets card->suit
+    void _setSuit(std::string s){
+        this->suit = s;
+    }
+
+    //returs card->value
+    char _getValue(){
+        return this->value;
+    }
+
+    ///sets card->value
+    void _setValue(char v){
+        this->value = v;
     }
 };
